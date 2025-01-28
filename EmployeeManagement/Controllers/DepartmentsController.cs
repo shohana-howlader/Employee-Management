cd0f85debc03f0e16dad7a15bb942db80cf01a61
shohana-howlader <166001432+shohana-howlader@users.noreply.github.com>
2025-01-28 21:47:15 +0600

    ok

---
+++ EmployeeManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,93 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DepartmentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments.Include(d => d.Manager).ToListAsync();
+            return View(departments);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Employees = _context.Employees.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Department department)
+        {
+
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null) return NotFound();
+
+            ViewBag.Employees = _context.Employees.ToList();
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Department department)
+        {
+            if (id != department.Id) return NotFound();
+
+
+            try
+            {
+                _context.Update(department);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Departments.Any(d => d.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var department = await _context.Departments.Include(d => d.Manager).FirstOrDefaultAsync(m => m.Id == id);
+            if (department == null) return NotFound();
+
+            return View(department);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
