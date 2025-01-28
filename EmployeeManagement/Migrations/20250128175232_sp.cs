bfd40f63c12bf38376e5ebb3238663c6b5c9bba8
shohana-howlader <166001432+shohana-howlader@users.noreply.github.com>
2025-01-28 23:55:01 +0600

    sp

---
+++ EmployeeManagement/Migrations/20250128175232_sp.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Models.Seed;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class sp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(DepartmentPerformanceScoreConstants.GetSql());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE GetDepartmentPerformanceScores;");
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Seed/DepartmentPerformanceScoreConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManagement.Models.Seed
+{
+    public static class DepartmentPerformanceScoreConstants
+    {
+        public const string ProcedureName = "GetDepartmentPerformanceScores";
+
+        public static string GetSql()
+        {
+            return @"
+        create PROCEDURE GetDepartmentPerformanceScores
+            AS
+            BEGIN
+                SELECT 
+                    d.Id,
+                    d.DepartmentName,
+                    AVG(pr.ReviewScore) AS AverageScore
+                FROM Departments d
+                JOIN Employees e ON d.Id = e.DepartmentId
+                JOIN PerformanceReviews pr ON e.Id = pr.EmployeeId
+                GROUP BY d.Id, d.DepartmentName
+                HAVING COUNT(pr.Id) > 0
+                ORDER BY AverageScore DESC;
+            END";
+        }
+    }
+}
